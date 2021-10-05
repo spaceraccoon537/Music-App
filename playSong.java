32f53a453aa9a2e60575f9a69e93b07b1f9f4b0f
Space Raccoon <60393406+spaceraccoon537@users.noreply.github.com>
2021-10-05 19:58:04 +0530

    Add files via upload
    These files can be used to replicate my project.
    
    Thank you

---
+++ playSong.java
@@ -0,0 +1,148 @@
+package com.example.memusic_fy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class playSong extends AppCompatActivity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        updateseek.interrupt();
+    }
+
+    TextView textView;
+    ImageView play,previous,next;
+    ArrayList<File> songs;
+    MediaPlayer mediaPlayer;
+    String textcontent;
+    int position;
+    SeekBar seekBar;
+    Thread updateseek;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+        textView=findViewById(R.id.textView);
+        play=findViewById(R.id.play);
+        previous=findViewById(R.id.previous);
+        next=findViewById(R.id.next);
+        seekBar=findViewById(R.id.seekBar);
+
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        songs=(ArrayList)bundle.getParcelableArrayList("songlist");
+        textcontent=intent.getStringExtra("currentsong");
+        textView.setText(textcontent);
+        textView.setSelected(true);
+        position=intent.getIntExtra("position",0);
+
+        Uri uri=Uri.parse(songs.get(position).toString());
+        mediaPlayer=MediaPlayer.create(this,uri);
+        mediaPlayer.start();
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+        updateseek=new Thread(){
+            @Override
+            public void run() {
+                int currentPos=0;
+                try{
+                    while(currentPos< mediaPlayer.getDuration()){
+                        currentPos=mediaPlayer.getCurrentPosition();
+                        seekBar.setProgress(currentPos);
+                        sleep(800);
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        updateseek.start();
+        play.setImageResource(R.drawable.pause);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()){
+                    play.setImageResource(R.drawable.play);
+                    mediaPlayer.pause();
+                }
+                else{
+                    play.setImageResource(R.drawable.pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                if(position!=0){
+                    position=position-1;
+                }
+                else{
+                    position=songs.size()-1;
+                }
+                Uri uri=Uri.parse(songs.get(position).toString());
+                mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
+                mediaPlayer.start();
+                play.setImageResource(R.drawable.pause);
+                seekBar.setMax(mediaPlayer.getDuration());
+                textcontent=songs.get(position).getName().toString();
+                textView.setText(textcontent);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                if(position!= songs.size()-1){
+                    position=position+1;
+                }
+                else{
+                    position=0;
+                }
+                Uri uri=Uri.parse(songs.get(position).toString());
+                mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
+                mediaPlayer.start();
+                play.setImageResource(R.drawable.pause);
+                seekBar.setMax(mediaPlayer.getDuration());
+                textcontent=songs.get(position).getName().toString();
+                textView.setText(textcontent);
+            }
+        });
+
+    }
+}
